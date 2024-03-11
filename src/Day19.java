dcf89066820b76b856ff037f6e8a2187bd47de4f
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-11 13:07:25 +0100

    Day 19 part 2.

--- src/Day19.java
+++ src/Day19.java
@@ -10,6 +10,7 @@ public class Day19 {
     public static void main(String[] args) throws IOException {
         Program program = new Program("input19.txt");
         first(program);
+        second(program);
     }
 
     private static void first(Program program) {
@@ -22,6 +23,41 @@ public class Day19 {
             }
         }
         System.out.println(count);
+    }
+
+    private static void second(Program program) {
+        boolean[][] positions = new boolean[2000][2000];
+        for (int y = 0; y < positions.length; y++) {
+            for (int x = 0; x < positions[0].length; x++) {
+                positions[y][x] = scan(program, x, y);
+            }
+        }
+        int[][] hroom = new int[positions.length][positions[0].length];
+        for (int y = 0; y < positions.length; y++) {
+            int counter = 0;
+            for (int x = positions[0].length - 1; x >= 0; x--) {
+                if (!positions[y][x]) {
+                    counter = 0;
+                } else {
+                    hroom[y][x] = ++counter;
+                }
+            }
+        }
+        for (int y = 0; y < positions.length - 99; y++) {
+            for (int x = 0; x < positions[0].length; x++) {
+                boolean ok = true;
+                for (int y1 = 0; y1 < 100; y1++) {
+                    if (hroom[y + y1][x] < 100) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) {
+                    System.out.println(x * 10000 + y);
+                    return;
+                }
+            }
+        }
     }
 
     private static boolean scan(Program program,int x,int y) {
