3e20069f9dc00ffd5ed022dbe85e463402b51357
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-11 11:11:07 +0100

    Day 22 part 2.

--- src/Day22.java
+++ src/Day22.java
@@ -1,6 +1,7 @@
 import lib.InputUtil;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,6 +10,7 @@ public class Day22 {
     public static void main(String[] args) throws IOException {
         List<String> input = InputUtil.readAsLines("input22.txt");
         first(input);
+        second(input);
     }
 
     private static void first(List<String> input) {
@@ -28,6 +30,30 @@ public class Day22 {
             if (deck.cards.get(i) == 2019) {
                 System.out.println(i);
             }
+        }
+    }
+
+    private static void second(List<String> input) {
+        BigInteger numberOfCards = BigInteger.valueOf(119315717514047L);
+        BigInteger times = BigInteger.valueOf(101741582076661L);
+        LinearFunction linearFunction = input.stream()
+                .map(s -> asLinearFunction(s, numberOfCards))
+                .reduce((a, b) -> a.compose(b, numberOfCards))
+                .orElseThrow();
+        linearFunction = linearFunction.repeat(times, numberOfCards);
+        System.out.println(linearFunction.apply(BigInteger.valueOf(2020L)).mod(numberOfCards));
+    }
+
+    private static LinearFunction asLinearFunction(String s,BigInteger numberOfCards) {
+        if ("deal into new stack".equals(s)) {
+            return new LinearFunction(BigInteger.ONE.negate(), BigInteger.ONE.negate().subtract(numberOfCards));
+        } else if (s.startsWith("cut")) {
+            return new LinearFunction(BigInteger.ONE, new BigInteger(s.substring(4)).mod(numberOfCards));
+        } else if (s.startsWith("deal with increment")) {
+            BigInteger z = new BigInteger(s.substring(20)).modInverse(numberOfCards);
+            return new LinearFunction(z.mod(numberOfCards), BigInteger.ZERO);
+        } else {
+            throw new IllegalArgumentException("Unknown shuffle instruction: " + s);
         }
     }
 
@@ -69,6 +95,41 @@ public class Day22 {
                 index = (index + n) % cards.size();
             }
             cards = newCards;
+        }
+    }
+
+    /**
+     * Represents linear function f(x)=ax+b
+     */
+    static class LinearFunction {
+        public static final LinearFunction IDENTITY = new LinearFunction(BigInteger.ONE, BigInteger.ZERO);
+
+        private final BigInteger a;
+        private final BigInteger b;
+
+        public LinearFunction(BigInteger a, BigInteger b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public BigInteger apply(BigInteger x) {
+            return a.multiply(x).add(b);
+        }
+
+        public LinearFunction compose(LinearFunction o,BigInteger modulo) {
+            return new LinearFunction(a.multiply(o.a).mod(modulo), a.multiply(o.b).add(b).mod(modulo));
+        }
+
+        public LinearFunction repeat(BigInteger times,BigInteger modulo) {
+            if (BigInteger.ZERO.equals(times)) {
+                return IDENTITY;
+            } else if (BigInteger.ZERO.equals(times.mod(BigInteger.TWO))) {
+                LinearFunction linearFunction = this.compose(this, modulo);
+                return linearFunction.repeat(times.divide(BigInteger.TWO), modulo);
+            } else {
+                LinearFunction linearFunction = repeat(times.subtract(BigInteger.ONE), modulo);
+                return compose(linearFunction, modulo);
+            }
         }
     }
 }
